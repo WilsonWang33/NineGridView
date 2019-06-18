bc24fde650473b552e660fac18313af96671e9e3
WilsonWang33 <599072484@qq.com>
2019-06-18 15:05:40 +0800

    ..

--- ninegridview/src/main/java/com/lzy/ninegrid/NineGridViewAdapter.java
+++ ninegridview/src/main/java/com/lzy/ninegrid/NineGridViewAdapter.java
@@ -1,51 +1,185 @@
-package com.lzy.ninegrid;
+package com.lzy.ninegrid.preview;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.Serializable;
+import com.lzy.ninegrid.ImageInfo;
+import com.lzy.ninegrid.NineGridView;
+import com.lzy.ninegrid.R;
+
+import java.io.File;
 import java.util.List;
 
-public abstract class NineGridViewAdapter implements Serializable {
+import uk.co.senab.photoview.PhotoView;
+import uk.co.senab.photoview.PhotoViewAttacher;
 
-    protected Context context;
+/**
+ * ================================================
+ * 作    者：廖子尧
+ * 版    本：1.0
+ * 创建日期：2016/3/21
+ * 描    述：
+ * 修订历史：
+ * ================================================
+ */
+public class ImagePreviewAdapter extends PagerAdapter implements PhotoViewAttacher.OnPhotoTapListener {
+
     private List<ImageInfo> imageInfo;
+    private Context context;
+    private View currentView;
 
-    public NineGridViewAdapter(Context context, List<ImageInfo> imageInfo) {
+    public ImagePreviewAdapter(Context context, @NonNull List<ImageInfo> imageInfo) {
+        super();
+        this.imageInfo = imageInfo;
         this.context = context;
-        this.imageInfo = imageInfo;
     }
 
-    /**
-     * 如果要实现图片点击的逻辑，重写此方法即可
-     *
-     * @param context      上下文
-     * @param nineGridView 九宫格控件
-     * @param index        当前点击图片的的索引
-     * @param imageInfo    图片地址的数据集合
-     */
-    protected void onImageItemClick(Context context, NineGridView nineGridView, int index, List<ImageInfo> imageInfo) {
+    @Override
+    public int getCount() {
+        return imageInfo.size();
     }
 
-    /**
-     * 生成ImageView容器的方式，默认使用NineGridImageViewWrapper类，即点击图片后，图片会有蒙板效果
-     * 如果需要自定义图片展示效果，重写此方法即可
-     *
-     * @param context 上下文
-     * @return 生成的 ImageView
-     */
-    protected ImageView generateImageView(Context context) {
-        NineGridViewWrapper imageView = new NineGridViewWrapper(context);
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        imageView.setImageResource(R.drawable.ic_default_color);
-        return imageView;
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
     }
 
-    public List<ImageInfo> getImageInfo() {
-        return imageInfo;
+    @Override
+    public void setPrimaryItem(ViewGroup container, int position, Object object) {
+        super.setPrimaryItem(container, position, object);
+        currentView = (View) object;
     }
 
-    public void setImageInfoList(List<ImageInfo> imageInfo) {
-        this.imageInfo = imageInfo;
+    public View getPrimaryItem() {
+        return currentView;
+    }
+
+    public ImageView getPrimaryImageView() {
+        return (ImageView) currentView.findViewById(R.id.pv);
+    }
+
+    public interface OnImageLongClickListener{
+        void onImageLongClick(Context context,String url);
+    }
+
+    OnImageLongClickListener onImageLongClickListener;
+
+    public void setOnImageLongClickListener(OnImageLongClickListener onImageLongClickListener) {
+        this.onImageLongClickListener = onImageLongClickListener;
+    }
+    
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_photoview, container, false);
+        final ProgressBar pb = (ProgressBar) view.findViewById(R.id.pb);
+        final PhotoView imageView = (PhotoView) view.findViewById(R.id.pv);
+
+        final ImageInfo info = this.imageInfo.get(position);
+        imageView.setOnPhotoTapListener(this);
+        showExcessPic(info, imageView);
+
+        //如果需要加载的loading,需要自己改写,不能使用这个方法
+        NineGridView.getImageLoader().onDisplayImage(view.getContext(), imageView, info.bigImageUrl);
+
+        final String url = info.bigImageUrl;
+//        pb.setVisibility(View.VISIBLE);
+//        Glide.with(context).load(info.bigImageUrl)//
+//                .placeholder(R.drawable.ic_default_image)//
+//                .error(R.drawable.ic_default_image)//
+//                .diskCacheStrategy(DiskCacheStrategy.ALL)//
+//                .listener(new RequestListener<String, GlideDrawable>() {
+//                    @Override
+//                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+//                        pb.setVisibility(View.GONE);
+//                        return false;
+//                    }
+//
+//                    @Override
+//                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+//                        pb.setVisibility(View.GONE);
+//                        return false;
+//                    }
+//                }).into(imageView);
+        imageView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    if(onImageLongClickListener!=null){
+                        onImageLongClickListener.onImageLongClick(context,url);
+                    }else{
+                        showSaveImgConfrim(context,url);
+                    }
+                    return false;
+                }
+         });
+        container.addView(view);
+        return view;
+    }
+    
+    private void showSaveImgConfrim(final Context context, final String url){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage("是否保存图片到本地？").setCancelable(true)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        DownPicUtil.downPic(url, new DownPicUtil.DownFinishListener() {
+                            @Override
+                            public void getDownPath(String s) {
+                                Toast.makeText(context,"图片已保存",Toast.LENGTH_SHORT).show();
+                                sendBoardCast(new File(s));
+                            }
+                        });
+                    }
+                }).setNegativeButton("取消",null).create().show();
+    }
+    
+    private void sendBoardCast(File file){
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri uri = Uri.fromFile(file);
+        intent.setData(uri);
+        context.sendBroadcast(intent);
+    }
+
+    /** 展示过度图片 */
+    private void showExcessPic(ImageInfo imageInfo, PhotoView imageView) {
+        //先获取大图的缓存图片
+        Bitmap cacheImage = NineGridView.getImageLoader().getCacheImage(imageInfo.bigImageUrl);
+        //如果大图的缓存不存在,在获取小图的缓存
+        if (cacheImage == null) cacheImage = NineGridView.getImageLoader().getCacheImage(imageInfo.thumbnailUrl);
+        //如果没有任何缓存,使用默认图片,否者使用缓存
+        if (cacheImage == null) {
+            imageView.setImageResource(R.drawable.ic_default_color);
+        } else {
+            imageView.setImageBitmap(cacheImage);
+        }
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    /** 单击屏幕关闭 */
+    @Override
+    public void onPhotoTap(View view, float x, float y) {
+        ((ImagePreviewActivity) context).finishActivityAnim();
     }
 }
